6a90e16372feaed9f710abde419a53b9ce20454a
unknown <vasylenko939@gmail.com>
2022-02-17 22:59:34 +0200

    Added ModulesVasylenko

---
+++ lab08/prj/ModulesVasylenko/main.cpp
@@ -0,0 +1,7 @@
+#include <cmath>
+double s_calculation(double x, double y, double z)
+{
+    double S;
+    S = z + M_PI*((pow((2*z + 1), 2) - sqrt(abs(y - 0.5*z))/sqrt(cos(z + y*z)+ pow(x, 2))));
+    return S;
+}
