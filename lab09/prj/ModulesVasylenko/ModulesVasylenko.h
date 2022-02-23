6d6f45c7b0fc38845ff8ea5feaa5462e6842c27e
unknown <vasylenko939@gmail.com>
2022-02-24 01:49:07 +0200

    Added ModulesVasylenko

---
+++ lab09/prj/ModulesVasylenko/ModulesVasylenko.h
@@ -0,0 +1,20 @@
+#ifndef MODULESVASYLENKO_H_INCLUDED
+#define MODULESVASYLENKO_H_INCLUDED
+#include <string>
+
+
+using namespace std;
+
+double s_calculation(double x, double y, double z);
+
+struct task_9_1
+{
+    short bonus = 0;
+    double pay = 0;
+};
+
+task_9_1 Bonus9_1(double price);
+float Temperature9_2(float t);
+int Binary9_3(int N);
+
+#endif // MODULESVASYLENKO_H_INCLUDED
